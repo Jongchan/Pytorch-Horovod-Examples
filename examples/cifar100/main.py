4c10110bf67329ad4e277c227192b61df29f336d
Ubuntu <lunit@batchai-test-template.xtgqefaghyduzhtxst2b2bbqad.bx.internal.cloudapp.net>
2018-10-02 06:59:14 +0000

    initial commit. main tested, horovod version not tested. (cifar100)

---
+++ examples/cifar100/main.py
@@ -0,0 +1,282 @@
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+import config as cf
+
+import torchvision
+import torchvision.transforms as transforms
+
+import os
+import sys
+import time
+import argparse
+import datetime
+
+from torch.autograd import Variable
+import numpy as np
+
+def conv3x3(in_planes, out_planes, stride=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=True)
+
+def conv_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        init.xavier_uniform(m.weight, gain=np.sqrt(2))
+        init.constant(m.bias, 0)
+    elif classname.find('BatchNorm') != -1:
+        init.constant(m.weight, 1)
+        init.constant(m.bias, 0)
+
+class wide_basic(nn.Module):
+    def __init__(self, in_planes, planes, dropout_rate, stride=1):
+        super(wide_basic, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, padding=1, bias=True)
+        self.dropout = nn.Dropout(p=dropout_rate)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=True)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=True),
+            )
+
+    def forward(self, x):
+        out = self.dropout(self.conv1(F.relu(self.bn1(x))))
+        out = self.conv2(F.relu(self.bn2(out)))
+        out += self.shortcut(x)
+
+        return out
+
+class Wide_ResNet(nn.Module):
+    def __init__(self, depth, widen_factor, dropout_rate, num_classes):
+        super(Wide_ResNet, self).__init__()
+        self.in_planes = 16
+
+        assert ((depth-4)%6 ==0), 'Wide-resnet depth should be 6n+4'
+        n = (depth-4)/6
+        k = widen_factor
+
+        print('| Wide-Resnet %dx%d' %(depth, k))
+        nStages = [16, 16*k, 32*k, 64*k]
+
+        self.conv1 = conv3x3(3,nStages[0])
+        self.layer1 = self._wide_layer(wide_basic, nStages[1], n, dropout_rate, stride=1)
+        self.layer2 = self._wide_layer(wide_basic, nStages[2], n, dropout_rate, stride=2)
+        self.layer3 = self._wide_layer(wide_basic, nStages[3], n, dropout_rate, stride=2)
+        self.bn1 = nn.BatchNorm2d(nStages[3], momentum=0.9)
+        self.linear = nn.Linear(nStages[3], num_classes)
+
+    def _wide_layer(self, block, planes, num_blocks, dropout_rate, stride):
+        strides = [stride] + [1]*int(num_blocks-1)
+        layers = []
+
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, dropout_rate, stride))
+            self.in_planes = planes
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = F.relu(self.bn1(out))
+        out = F.avg_pool2d(out, 8)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+
+        return out
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR-100 Training')
+parser.add_argument('--lr', default=0.1, type=float, help='learning_rate')
+parser.add_argument('--depth', default=28, type=int, help='depth of model')
+parser.add_argument('--widen_factor', default=10, type=int, help='width of model')
+parser.add_argument('--dropout', default=0.3, type=float, help='dropout_rate')
+parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+parser.add_argument('--testOnly', '-t', action='store_true', help='Test mode with the saved model')
+args = parser.parse_args()
+
+# Hyper Parameter settings
+use_cuda = torch.cuda.is_available()
+best_acc = 0
+start_epoch, num_epochs, batch_size, optim_type = cf.start_epoch, cf.num_epochs, cf.batch_size, cf.optim_type
+
+# Data Uplaod
+print('\n[Phase 1] : Data Preparation')
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(cf.mean['cifar100'], cf.std['cifar100']),
+]) # meanstd transformation
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(cf.mean['cifar100'], cf.std['cifar100']),
+])
+
+print("| Preparing CIFAR-100 dataset...")
+sys.stdout.write("| ")
+trainset = torchvision.datasets.CIFAR100(root='/home/lunit/smbmount', train=True, download=False, transform=transform_train)
+testset = torchvision.datasets.CIFAR100(root='/home/lunit/smbmount', train=False, download=False, transform=transform_test)
+num_classes = 100
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+
+# Return network & file name
+def getNetwork(args):
+    net = Wide_ResNet(args.depth, args.widen_factor, args.dropout, num_classes)
+    file_name = 'wide-resnet-'+str(args.depth)+'x'+str(args.widen_factor)
+
+    return net, file_name
+
+# Test only option
+if (args.testOnly):
+    print('\n[Test Phase] : Model setup')
+    assert os.path.isdir('checkpoint'), 'Error: No checkpoint directory found!'
+    _, file_name = getNetwork(args)
+    checkpoint = torch.load('./checkpoint/'+os.sep+file_name+'.t7')
+    net = checkpoint['net']
+
+    if use_cuda:
+        net.cuda()
+        net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
+        cudnn.benchmark = True
+
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+
+    for batch_idx, (inputs, targets) in enumerate(testloader):
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()
+        inputs, targets = Variable(inputs, volatile=True), Variable(targets)
+        outputs = net(inputs)
+
+        _, predicted = torch.max(outputs.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum()
+
+    acc = 100.*correct/total
+    print("| Test Result\tAcc@1: %.2f%%" %(acc))
+
+    sys.exit(0)
+
+# Model
+print('\n[Phase 2] : Model setup')
+if args.resume:
+    # Load checkpoint
+    print('| Resuming from checkpoint...')
+    assert os.path.isdir('checkpoint'), 'Error: No checkpoint directory found!'
+    _, file_name = getNetwork(args)
+    checkpoint = torch.load('./checkpoint/'+os.sep+file_name+'.t7')
+    net = checkpoint['net']
+    best_acc = checkpoint['acc']
+    start_epoch = checkpoint['epoch']
+else:
+    print('| Building net ...')
+    net, file_name = getNetwork(args)
+    net.apply(conv_init)
+
+if use_cuda:
+    net.cuda()
+    net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
+    cudnn.benchmark = True
+
+criterion = nn.CrossEntropyLoss()
+
+# Training
+def train(epoch):
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    optimizer = optim.SGD(net.parameters(), lr=cf.learning_rate(args.lr, epoch), momentum=0.9, weight_decay=5e-4)
+
+    print('\n=> Training Epoch #%d, LR=%.4f' %(epoch, cf.learning_rate(args.lr, epoch)))
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda() # GPU settings
+        optimizer.zero_grad()
+        inputs, targets = Variable(inputs), Variable(targets)
+        outputs = net(inputs)               # Forward Propagation
+        loss = criterion(outputs, targets)  # Loss
+        loss.backward()  # Backward Propagation
+        optimizer.step() # Optimizer update
+
+        train_loss += loss.data[0]
+        _, predicted = torch.max(outputs.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum()
+
+        sys.stdout.write('\r')
+        sys.stdout.write('| Epoch [%3d/%3d] Iter[%3d/%3d]\t\tLoss: %.4f Acc@1: %.3f%%'
+                %(epoch, num_epochs, batch_idx+1,
+                    (len(trainset)//batch_size)+1, loss.data[0], 100.*correct/total))
+        sys.stdout.flush()
+
+def test(epoch):
+    global best_acc
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(testloader):
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()
+        inputs, targets = Variable(inputs, volatile=True), Variable(targets)
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+
+        test_loss += loss.data[0]
+        _, predicted = torch.max(outputs.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum()
+
+    # Save checkpoint when best model
+    acc = 100.*correct/total
+    print("\n| Validation Epoch #%d\t\t\tLoss: %.4f Acc@1: %.2f%%" %(epoch, loss.data[0], acc))
+
+    if acc > best_acc:
+        print('| Saving Best model...\t\t\tTop1 = %.2f%%' %(acc))
+        state = {
+                'net':net.module if use_cuda else net,
+                'acc':acc,
+                'epoch':epoch,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        save_point = './checkpoint/'+os.sep
+        if not os.path.isdir(save_point):
+            os.mkdir(save_point)
+        torch.save(state, save_point+file_name+'.t7')
+        best_acc = acc
+
+print('\n[Phase 3] : Training model')
+print('| Training Epochs = ' + str(num_epochs))
+print('| Initial Learning Rate = ' + str(args.lr))
+print('| Optimizer = ' + str(optim_type))
+
+elapsed_time = 0
+for epoch in range(start_epoch, start_epoch+num_epochs):
+    start_time = time.time()
+
+    train(epoch)
+    test(epoch)
+
+    epoch_time = time.time() - start_time
+    elapsed_time += epoch_time
+    print('| Elapsed time : %d:%02d:%02d'  %(cf.get_hms(elapsed_time)))
+
+print('\n[Phase 4] : Testing model')
+print('* Test results : Acc@1 = %.2f%%' %(best_acc))
