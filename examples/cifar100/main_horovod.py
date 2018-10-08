ddf3b0272a92fe416d1ea121922df8f770de37cb
Jongchan Park <jongchan@kaist.ac.kr>
2018-10-08 10:45:28 +0900

    Update main_horovod.py

--- examples/cifar100/main_horovod.py
+++ examples/cifar100/main_horovod.py
@@ -118,7 +118,7 @@ use_cuda = torch.cuda.is_available()
 if use_cuda:
     print ("use cuda!!")
     print ("local rank {}".format(hvd.local_rank()))
-    torch.cuda.set_device(hvd.local_rank())
+    torch.cuda.set_device(hvd.local_rank()*torch.cuda.device_count())
     torch.cuda.manual_seed(1111)
 
 best_acc = 0
